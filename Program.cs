8e83cacd249256bb9e1950f2e2089c5c8ae0e7e6
WinZawOo <winzawoo.dev@gmail.com>
2025-03-16 23:31:15 +0630

    test api endpoint and add middleware

--- Program.cs
+++ Program.cs
@@ -1,9 +1,21 @@
 using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Rewrite;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+
+//Middlewares 
+app.UseRewriter(new RewriteOptions().AddRedirect("tasks/(.*)", "todos/$1"));
+app.Use( async( context, next) => {
+    Console.WriteLine($"[{context.Request.Method} {context.Request.Path} {DateTime.UtcNow} started!]");
+    await next();
+    Console.WriteLine($"[{context.Request.Method} {context.Request.Path} {DateTime.UtcNow} finished!]");
+});
+
 var todos = new List<ToDo>();
+
+app.MapGet("/", () => "Hello World!");
 
 app.MapGet("/todos", () => TypedResults.Ok(todos));
 
@@ -22,8 +34,10 @@ app.MapPost("/todos", (ToDo todo) =>
     return TypedResults.Created($"/todos/{todo.Id}", todo);
 });
 
-app.MapPut("todos/{id}", Results<Ok<ToDo>, NotFound> (int id, ToDo todoParams) =>
+app.MapPut("/todos/{id}", Results<Ok<ToDo>, NotFound> (int id, ToDo todoInput) =>
 {
+
+    System.Console.WriteLine($"id: {id}");
 
     var todo = todos.SingleOrDefault(t => t.Id == id);
 
@@ -33,11 +47,11 @@ app.MapPut("todos/{id}", Results<Ok<ToDo>, NotFound> (int id, ToDo todoParams) =
     }
 
     todos.Remove(todo);
-    todos.Add(todoParams);
+    todos.Add(todoInput);
     return TypedResults.Ok(todo);
 });
 
-app.MapDelete("todos/{id}", Results<NoContent, NotFound> (int id) =>
+app.MapDelete("/todos/{id}", Results<NoContent, NotFound> (int id) =>
 {
     var todo = todos.SingleOrDefault(t => t.Id == id);
 

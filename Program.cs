2a64a37eab1e5dbf03ea6ad7dc9224da86f0ba5c
WinZawOo <winzawoo.dev@gmail.com>
2025-03-17 11:37:56 +0630

    Dependency Injection

--- Program.cs
+++ Program.cs
@@ -2,8 +2,10 @@ using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Rewrite;
 
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
+builder.Services.AddSingleton<ITaskService>(new InMemoryTaskService());
+
+var app = builder.Build();
 
 //Middlewares 
 app.UseRewriter(new RewriteOptions().AddRedirect("tasks/(.*)", "todos/$1"));
@@ -14,26 +16,24 @@ app.Use(async (context, next) =>
     Console.WriteLine($"[{context.Request.Method} {context.Request.Path} {DateTime.UtcNow} finished!]");
 });
 
-var todos = new List<ToDo>();
-
 app.MapGet("/", () => "Hello World!");
 
-app.MapGet("/todos", () => TypedResults.Ok(todos));
+app.MapGet("/todos", (ITaskService service) => TypedResults.Ok(service.GetToDos()));
 
-app.MapGet("/todos/{id}", Results<Ok<ToDo>, NotFound> (int id) =>
+app.MapGet("/todos/{id}", Results<Ok<ToDo>, NotFound> (int id, ITaskService service) =>
 {
 
-    var todo = todos.SingleOrDefault(t => t.Id == id);
-
+    var todo = service.GetToDoById(id);
     return todo is null ? TypedResults.NotFound() : TypedResults.Ok(todo);
 
 });
 
-app.MapPost("/todos", (ToDo todo) =>
+app.MapPost("/todos", (ToDo todo, ITaskService service) =>
 {
     System.Console.WriteLine(todo);
-    todos.Add(todo);
-    return TypedResults.Created($"/todos/{todo.Id}", todo);
+    var task = service.AddToDo(todo);
+    return TypedResults.Created($"/todos/{todo.Id}", task);
+
 }).AddEndpointFilter(async (context, next) =>
 {
     var taskArgument = context.GetArgument<ToDo>(0);
@@ -63,35 +63,77 @@ app.MapPost("/todos", (ToDo todo) =>
     return await next(context);
 });
 
-app.MapPut("/todos/{id}", Results<Ok<ToDo>, NotFound> (int id, ToDo todoInput) =>
+app.MapPut("/todos/{id}", Results<Ok<ToDo>, NotFound> (int id, ToDo todoInput, ITaskService service) =>
 {
 
     System.Console.WriteLine($"id: {id}");
 
-    var todo = todos.SingleOrDefault(t => t.Id == id);
+    var todo = service.GetToDoById(id);
 
     if (todo is null)
     {
         return TypedResults.NotFound();
     }
 
-    todos.Remove(todo);
-    todos.Add(todoInput);
+    service.DeleteTodoById(id);
+    service.AddToDo(todoInput);
+
     return TypedResults.Ok(todo);
 });
 
-app.MapDelete("/todos/{id}", Results<NoContent, NotFound> (int id) =>
+app.MapDelete("/todos/{id}", Results<NoContent, NotFound> (int id, ITaskService service) =>
 {
-    var todo = todos.SingleOrDefault(t => t.Id == id);
+    var todo = service.GetToDoById(id);
 
     if (todo is null)
     {
         return TypedResults.NotFound();
     }
-    todos.Remove(todo);
+
+    service.DeleteTodoById(id);
     return TypedResults.NoContent();
 });
 
 app.Run();
 
 public record ToDo(int Id, string Name, DateTime DueDate, bool IsCompleted);
+
+interface ITaskService
+{
+    ToDo? GetToDoById(int id);
+
+    List<ToDo> GetToDos();
+
+    void DeleteTodoById(int id);
+
+    ToDo AddToDo(ToDo task);
+}
+
+public class InMemoryTaskService : ITaskService
+{
+    private List<ToDo> todos = new List<ToDo>();
+
+    public ToDo GetToDoById(int id)
+    {
+        var task = todos.SingleOrDefault(t => t.Id == id);
+        return task!;
+    }
+
+    public List<ToDo> GetToDos()
+    {
+        return todos;
+    }
+
+    public void DeleteTodoById(int id)
+    {
+        var task = todos.SingleOrDefault(t => t.Id == id);
+        todos.Remove(task!);
+    }
+
+    public ToDo AddToDo(ToDo task)
+    {
+        todos.Add(task);
+        return task;
+    }
+
+}

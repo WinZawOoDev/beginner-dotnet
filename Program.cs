cb642cd489038c0b22bb2162ca74b76cdfeecce7
WinZawOo <winzawoo.dev@gmail.com>
2025-03-17 10:43:41 +0630

    Endpoint filter and validations

--- Program.cs
+++ Program.cs
@@ -7,7 +7,8 @@ var app = builder.Build();
 
 //Middlewares 
 app.UseRewriter(new RewriteOptions().AddRedirect("tasks/(.*)", "todos/$1"));
-app.Use( async( context, next) => {
+app.Use(async (context, next) =>
+{
     Console.WriteLine($"[{context.Request.Method} {context.Request.Path} {DateTime.UtcNow} started!]");
     await next();
     Console.WriteLine($"[{context.Request.Method} {context.Request.Path} {DateTime.UtcNow} finished!]");
@@ -30,8 +31,36 @@ app.MapGet("/todos/{id}", Results<Ok<ToDo>, NotFound> (int id) =>
 
 app.MapPost("/todos", (ToDo todo) =>
 {
+    System.Console.WriteLine(todo);
     todos.Add(todo);
     return TypedResults.Created($"/todos/{todo.Id}", todo);
+}).AddEndpointFilter(async (context, next) =>
+{
+    var taskArgument = context.GetArgument<ToDo>(0);
+    var errors = new Dictionary<string, string[]>();
+
+    // System.Console.WriteLine(taskArgument);
+
+    // System.Console.WriteLine(nameof(taskArgument.DueDate));
+
+    // System.Console.WriteLine(taskArgument.IsCompleted);
+
+    if (taskArgument.DueDate < DateTime.UtcNow)
+    {
+        errors.Add(nameof(taskArgument.DueDate), ["Due date must be in the future"]);
+    }
+
+    if (taskArgument.IsCompleted)
+    {
+        errors.Add(nameof(taskArgument.IsCompleted), ["Cannot add completed todo"]);
+    }
+
+    if (errors.Count > 0)
+    {
+        return Results.ValidationProblem(errors);
+    }
+
+    return await next(context);
 });
 
 app.MapPut("/todos/{id}", Results<Ok<ToDo>, NotFound> (int id, ToDo todoInput) =>
